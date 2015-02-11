93e74149be61d9eb570edf96952173f20fabf4d7
Joel West <Joel.West@banno.com>
2015-02-11 15:32:20 -0600

    Added Button Colors

--- Will/JJWHabitCardEditView.h
+++ Will/JJWHabitCardEditView.h
@@ -10,6 +10,14 @@
 
 @interface JJWHabitCardEditView : UIView
 
+@property (nonatomic) IBOutlet UIButton *sundayButton;
+@property (nonatomic) IBOutlet UIButton *mondayButton;
+@property (nonatomic) IBOutlet UIButton *tuesdayButton;
+@property (nonatomic) IBOutlet UIButton *wednesdayButton;
+@property (nonatomic) IBOutlet UIButton *thursdayButton;
+@property (nonatomic) IBOutlet UIButton *fridayButton;
+@property (nonatomic) IBOutlet UIButton *saturdayButton;
+
 + (instancetype)viewFromNib;
 
 @end
